78ef7d75228d26f65313c43d45bbdd11a9715f56
Sidnei Pereira <sidi.jp@gmail.com>
2020-05-08 23:25:13 -0300

    House keeping

--- webblocker.py
+++ webblocker.py
@@ -1,34 +1,53 @@
 # coding: utf-8
-
 import time
 from datetime import datetime as dt
 
+
 def get_hosts_file():
-    #hosts_file = r'C:\Windows\System32\drivers\etc\hosts'
+    # hosts_file = r'C:\Windows\System32\drivers\etc\hosts'
     hosts_file = r'/etc/hosts'
     return hosts_file
 
+
 HOSTS_FILE = get_hosts_file()
-WEBSITE_LIST = ['www.facebook.com', 'facebook.com', 'gmail.com', 'linkedin.com', 'www.linkedin.com']
+WEBSITE_LIST = [
+    'instagram.com',
+    'www.instagram.com',
+    'www.facebook.com',
+    'facebook.com',
+    'linkedin.com',
+    'www.linkedin.com',
+    'www.mercadolivre.com.br',
+    'mercadolivre.com.br',
+    'mail.google.com',
+    'www.gmail.com',
+    'gmail.com',
+    'www.muambator.com.br',
+    'muambator.com.br',
+    'twitter.com',
+    'www.twitter.com',
+]
+
 REDIRECT_ADDRESS = '127.0.0.1'
 
 PERIODS = [
     {
-        'start_time': '9:00',
-        'end_time': '12:00',
+        'start_time': '7:00',
+        'end_time': '12:30',
         'website_list': WEBSITE_LIST,
     },
     {
-        'start_time': '11:00',
+        'start_time': '13:00',
         'end_time': '18:00',
         'website_list': WEBSITE_LIST,
     },
     {
-        'start_time': '19:30',
-        'end_time': '23:30',
+        'start_time': '18:30',
+        'end_time': '23:00',
         'website_list': ['facebook.com', 'www.facebook.com'],
     },
 ]
+
 
 def setup_periods(periods):
     for period in periods:
@@ -36,20 +55,26 @@ def setup_periods(periods):
         period['end_time'] = get_time_from_string(period['end_time'])
         period['state'] = False
 
+
 def get_time_from_string(string_time):
     '''string_time format hh:mm (e.g., 18:00)'''
     hour, minute = string_time.split(':')
     hour, minute = int(hour), int(minute)
     return dt.now().replace(hour=hour, minute=minute)
 
+
 def delta_seconds(end_time, start_time):
     wait_for = (end_time - start_time).total_seconds()
     return int(wait_for)
 
+
 def in_period(moment, period):
-    period['start_time'] = period['start_time'].replace(year=moment.year, month=moment.month, day=moment.day, second=0)
-    period['end_time'] = period['end_time'].replace(year=moment.year, month=moment.month, day=moment.day, second=59)
+    period['start_time'] = period['start_time'].replace(
+        year=moment.year, month=moment.month, day=moment.day, second=0)
+    period['end_time'] = period['end_time'].replace(
+        year=moment.year, month=moment.month, day=moment.day, second=59)
     return period['start_time'] < moment < period['end_time']
+
 
 def remove_websites_from_hosts(file, website_list):
     with open(file, 'r+') as f:
@@ -57,34 +82,33 @@ def remove_websites_from_hosts(file, website_list):
         f.seek(0)
         for line in content:
             if not any(website in line for website in website_list):
-                # print('unblocked!!!!!')
                 f.write(line)
         f.truncate()
 
+
 def add_websites_to_hosts(file, website_list):
-    # print('blocking')
     with open(file, 'r+') as f:
         content = f.read()
         for website in website_list:
             if website not in content:
                 f.write(REDIRECT_ADDRESS + ' ' + website + '\n')
 
+
 def main():
     setup_periods(PERIODS)
     while True:
         for period in PERIODS:
-            if period['state'] == False and in_period(dt.now(), period):
+            if period['state'] is False and in_period(dt.now(), period):
                 period['state'] = True
                 add_websites_to_hosts(HOSTS_FILE, period['website_list'])
                 wait_for = delta_seconds(period['end_time'], dt.now())
-                # print(wait_for)
                 time.sleep(wait_for)
                 break
             elif period['state']:
                 period['state'] = False
                 remove_websites_from_hosts(HOSTS_FILE, period['website_list'])
-        # print('not blocking')
         time.sleep(60)
+
 
 if __name__ == '__main__':
     main()
